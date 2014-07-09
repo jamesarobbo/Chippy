275be2e51c4b87a3fae4032f842b03047dc0316f
James Robinson <jamesarobinson6@gmail.com>
2014-07-09 21:41:07 +0100

    amendments to admin order product edit form

--- app/admin/order_product.rb
+++ app/admin/order_product.rb
@@ -19,11 +19,25 @@ controller do
 
     super do |success,failure|
       success.html {redirect_to admin_order_path(order)}
+
     end  
   end
-
-
 end 
+
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs "Edit Quantity", :multipart => true do
+     
+        opsizes = OrderProduct.find(params[:id]).product.sizes
+        
+        f.input :size, as: :select, collection: opsizes
+        f.input :quantity
+      end
+      f.actions do
+        order = OrderProduct.find(params[:id]).order
+        f.action(:submit)
+        f.cancel_link(admin_order_path(order))
+      end
+  end
   
 
 end
